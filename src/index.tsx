1751b971a97e0b0729812c8b7e87cf9d197b0b87
xuejian.xu01 <xuejian.xu01@liulishuo.com>
2021-02-20 14:30:30 +0800

    init repo

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>hello world</h1>
   </React.StrictMode>,
   document.getElementById('root'),
 );
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
